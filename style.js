09a9c09b0be5bfaac0534d856e269a78bbb629d2
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-09-25 13:03:11 +0200

    responsive code added

--- style.js
+++ style.js
@@ -110,3 +110,19 @@ document.getElementById(promptLocation).innerHTML = message;
 document.getElementById(promptLocation).style.color = color;
 
 }
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+  menu.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('active');
+}
